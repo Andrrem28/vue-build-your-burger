8652c24d2f1beed2b9bf965a8e90fad4e8803914
Andrrem28 <andree.martins_@hotmail.com>
2024-02-19 22:48:45 -0300

    styling the navigation bar and footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,11 +1,35 @@
 <template>
-  <p>Feito por Barquinho's Software.</p>
+  <div id="footer">
+    <p>
+      Feito por Barquinho's Software &copy; <strong>{{ currentYear }}</strong
+      >.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Footer-html",
+  data() {
+    return {
+      currentYear: null,
+    };
+  },
+  mounted() {
+    this.currentYear = new Date().getFullYear();
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#footer {
+  height: 100px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+</style>
--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link :to="{ path: '/' }">
-      <img src="" alt="" id="logo" />
+    <router-link :to="{ path: '/' }" id="logo_url">
+      <img :src="logo" :alt="alt" id="logo" />
     </router-link>
     <router-link to="/">Home</router-link> |
     <router-link to="/orders">Pedidos</router-link>
@@ -11,7 +11,35 @@
 <script>
 export default {
   name: "Navbar-html",
+  props: ["logo", "alt"],
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#nav {
+  background-color: #222;
+  border-bottom: 4px solid #111;
+  padding: 15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+#nav #logo_url {
+  margin: auto;
+  margin-left: 0;
+}
+#logo {
+  width: 40px;
+  height: 40px;
+}
+#nav a {
+  color: #fcba03;
+  text-decoration: none;
+  margin: 12px;
+  transition: 0.5s;
+}
+
+#nav a:hover {
+  color: #fff;
+}
+</style>

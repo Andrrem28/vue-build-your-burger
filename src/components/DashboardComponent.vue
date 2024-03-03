37b3a02028769fa044e3c07dab64f84c27ef6c24
Andrrem28 <andree.martins_@hotmail.com>
2024-03-03 20:40:05 -0300

    make dashboard componente

---
+++ src/components/DashboardComponent.vue
@@ -0,0 +1,116 @@
+<template>
+  <div id="burger-table">
+    <div>
+      <div id="burger-table-heading">
+        <div class="order-id">#:</div>
+        <div>Cliente:</div>
+        <div>Pão:</div>
+        <div>Carne:</div>
+        <div>Opcionais:</div>
+        <div>Ações:</div>
+      </div>
+    </div>
+    <div id="burger-table-rows">
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.name }}</div>
+        <div>{{ burger.bread }}</div>
+        <div>{{ burger.meat }}</div>
+        <div>
+          <ul>
+            <li v-for="(additional, index) in burger.more_options" :key="index">
+              {{ additional }}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <select name="status" class="status">
+            <option>Selecione...</option>
+          </select>
+          <button class="delete-btn">Cancelar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Dashboard-component",
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+    };
+  },
+  methods: {
+    async getOrders() {
+      const request = await fetch("http://localhost:3000/burgers");
+      const data = await request.json();
+
+      this.burgers = data;
+    },
+  },
+  mounted() {
+    this.getOrders();
+  },
+};
+</script>
+
+<style scoped>
+#burger-table {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+#burger-table-heading,
+#burger-table-rows,
+.burger-table-row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#burger-table-heading {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
+}
+
+.burger-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
+}
+
+#burger-table-heading div,
+.burger-table-row div {
+  width: 19%;
+}
+
+#burger-table-heading .order-id,
+.burger-table-row .order-number {
+  width: 5%;
+}
+
+select {
+  padding: 12px 6px;
+  margin-right: 12px;
+}
+
+.delete-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
+</style>

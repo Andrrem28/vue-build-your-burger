309f47370118e20b40d7e404badca94f8492e55c
Andrrem28 <andree.martins_@hotmail.com>
2024-03-03 20:40:25 -0300

    make order component

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="main-container">
+    <h1>Gerenciar pedidos.</h1>
     <h1>Pedidos</h1>
+    <DashboardComponent />
   </div>
 </template>
+
+<script>
+import DashboardComponent from "@/components/DashboardComponent.vue";
+export default {
+  name: "Orders-html",
+  components: {
+    DashboardComponent,
+  },
+};
+</script>
